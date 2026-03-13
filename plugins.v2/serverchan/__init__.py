b0ee2c927db864b6e9fd715bfe25a57221f142db
SilentReed <34358345+SilentReed@users.noreply.github.com>
2026-03-14 03:17:49 +0800

    Update plugin icon URL for Server酱³通知

--- plugins.v2/serverchan/__init__.py
+++ plugins.v2/serverchan/__init__.py
@@ -14,7 +14,7 @@ class ServerChan(_PluginBase):
     # 插件描述
     plugin_desc = "通过Server酱³发送消息通知，支持APP推送"
     # 插件图标
-    plugin_icon = "https://sc3.ft07.com/favicon.png"
+    plugin_icon = "icons/serverchan.png"
     # 插件版本
     plugin_version = "1.1.0"
     # 插件作者
